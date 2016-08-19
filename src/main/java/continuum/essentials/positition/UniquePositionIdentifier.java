0bd904eb108f608359e9b79e8a7219dff8a08b79
Madsthunder <madsthunder109@gmail.com>
2016-08-18 22:50:04 -0400

    Rewrite UniquePositionIdentifier

--- src/main/java/continuum/essentials/positition/UniquePositionIdentifier.java
+++ src/main/java/continuum/essentials/positition/UniquePositionIdentifier.java
@@ -1,34 +1,47 @@
 package continuum.essentials.positition;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
-public class UniquePositionIdentifier
+public class UniquePositionIdentifier extends BlockPos
 {
 	private final IBlockAccess access;
-	private final BlockPos pos;
+	
+	public UniquePositionIdentifier(UniquePositionIdentifier upid, EnumFacing facing)
+	{
+		this(upid, facing, 1);
+	}
+	
+	public UniquePositionIdentifier(UniquePositionIdentifier upid, EnumFacing facing, int amount)
+	{
+		this(upid.getBlockAccess(), upid.offset(facing, amount));
+	}
+	
+	public UniquePositionIdentifier(IBlockAccess access, int x, int y, int z)
+	{
+		super(x, y, z);
+		this.access = access;
+	}
 	
 	public UniquePositionIdentifier(IBlockAccess access, BlockPos pos)
 	{
-		this.access = access;
-		this.pos = pos;
-	}
-	
-	@Override
-	public final int hashCode()
-	{
-		return this.getBlockPos().hashCode();
+		this(access, pos.getX(), pos.getY(), pos.getZ());
 	}
 	
 	@Override
 	public final boolean equals(Object obj)
 	{
-		if(!(obj instanceof UniquePositionIdentifier))
+		if(super.equals(obj))
+			if(obj instanceof UniquePositionIdentifier)
+				return ((UniquePositionIdentifier)obj).getBlockAccess() == this.getBlockAccess();
+			else
+				return true;
+		else
 			return false;
-		UniquePositionIdentifier upid = (UniquePositionIdentifier)obj;
-		return upid.getBlockPos().equals(this.getBlockPos()) && upid.getBlockAccess() == this.getBlockAccess();
 	}
 	
 	public final IBlockAccess getBlockAccess()
@@ -36,19 +49,14 @@ public class UniquePositionIdentifier
 		return this.access;
 	}
 	
-	public final BlockPos getBlockPos()
-	{
-		return this.pos;
-	}
-	
 	public IBlockState getBlockState()
 	{
-		return this.getBlockAccess().getBlockState(this.getBlockPos());
+		return this.getBlockAccess().getBlockState(this);
 	}
 	
 	public IBlockState getActualState()
 	{
-		return this.getBlockAccess().getBlockState(this.getBlockPos()).getActualState(this.getBlockAccess(), this.getBlockPos());
+		return this.getBlockAccess().getBlockState(this).getActualState(this.getBlockAccess(), this);
 	}
 	
 	public final Boolean hasTileEntity()
@@ -58,6 +66,31 @@ public class UniquePositionIdentifier
 	
 	public TileEntity getTileEntity()
 	{
-		return this.getBlockAccess().getTileEntity(pos);
+		return this.getBlockAccess().getTileEntity(this);
+	}
+	
+	public boolean isBlockInstanceOf(Class clasz)
+	{
+		return clasz.isInstance(this.getBlockState().getBlock());
+	}
+	
+	public boolean isTileEntityInstanceOf(Class clasz)
+	{
+		return clasz.isInstance(this.getTileEntity());
+	}
+	
+	public Class<? extends Block> getBlockClass()
+	{
+		return this.getBlockState().getBlock().getClass();
+	}
+	
+	public Class<? extends TileEntity> getTileEntityClass()
+	{
+		return this.getTileEntity().getClass();
+	}
+	
+	public boolean blockEquals(Block block)
+	{
+		return this.getBlockState().getBlock() == block;
 	}
 }
