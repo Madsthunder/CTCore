62b5f54bfd6862bfbabd704b705d5592aea7a307
Madsthunder <madsthunder109@gmail.com>
2016-08-18 22:53:01 -0400

    Add getDirectionFromAxis

--- src/main/java/continuum/essentials/hooks/BlockHooks.java
+++ src/main/java/continuum/essentials/hooks/BlockHooks.java
@@ -5,6 +5,8 @@ import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.BlockSnapshot;
@@ -37,5 +39,17 @@ public class BlockHooks
 		for(Integer i : ObjectHooks.increment(items.length))
 			blocks[i] = Block.getBlockFromItem(items[i]);
 		return blocks;
+	}
+	
+	public static EnumFacing[] getFacingsFromAxis(Axis axis)
+	{
+		if(axis == null)
+			return new EnumFacing[0];
+		EnumFacing[] facings = new EnumFacing[2];
+		int i = 0;
+		for(EnumFacing facing : EnumFacing.values())
+			if(facing.getAxis() == axis)
+				facings[i++] = facing;
+		return facings;
 	}
 }
