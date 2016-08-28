d75fd35c680d17c347f915f400a997d949a8442d
Madsthunder <madsthunder109@gmail.com>
2016-08-28 15:01:11 -0400

    Add IConnectable

---
+++ src/main/java/continuum/essentials/block/IConnectable.java
@@ -0,0 +1,22 @@
+package continuum.essentials.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public interface IConnectable
+{
+	public boolean canConnectTo(IBlockAccess access, BlockPos pos, EnumFacing direction);
+	
+	public static boolean isConnectedTo(IConnectable connectable, IBlockAccess access, BlockPos pos, EnumFacing direction)
+	{
+		return connectable.canConnectTo(access, pos, direction) && canOtherConnect(access, pos, direction);
+	}
+	
+	public static boolean canOtherConnect(IBlockAccess access, BlockPos pos, EnumFacing direction)
+	{
+		Block block;
+		return (block = access.getBlockState(pos.offset(direction)).getBlock()) instanceof IConnectable ? ((IConnectable)block).canConnectTo(access, pos.offset(direction), direction.getOpposite()) : true;
+	}
+}
