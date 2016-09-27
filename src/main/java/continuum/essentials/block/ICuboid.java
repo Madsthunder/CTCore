e3cd993adf9ab85ac7ce6a8cc428ea98d7911653
Madsthunder <madsthunder109@gmail.com>
2016-09-27 16:40:06 -0400

    Added some useful ICuboid to AxisAlignedBB Functions.

--- src/main/java/continuum/essentials/block/ICuboid.java
+++ src/main/java/continuum/essentials/block/ICuboid.java
@@ -1,9 +1,28 @@
 package continuum.essentials.block;
 
+import com.google.common.base.Function;
+
 import net.minecraft.util.math.AxisAlignedBB;
 
 public interface ICuboid
 {
+	public static final Function<ICuboid, AxisAlignedBB> CUBOID_TO_SELECTABLE = new Function<ICuboid, AxisAlignedBB>()
+	{
+		@Override
+		public AxisAlignedBB apply(ICuboid cuboid)
+		{
+			return cuboid.getSelectableCuboid();
+		}
+	};
+	
+	public static final Function<ICuboid, AxisAlignedBB> CUBOID_TO_SHOWABLE = new Function<ICuboid, AxisAlignedBB>()
+	{
+		@Override
+		public AxisAlignedBB apply(ICuboid cuboid)
+		{
+			return cuboid.getShowableCuboid();
+		}
+	};
 	public AxisAlignedBB getSelectableCuboid();
 	
 	public AxisAlignedBB getShowableCuboid();
