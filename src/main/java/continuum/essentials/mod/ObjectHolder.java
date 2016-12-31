63a4416396ab2b3f8b4b2576e80db5e05826f3eb
Madsthunder <madsthunder109@gmail.com>
2016-12-31 14:16:58 -0500

    Added a simpler way for creating multiple items of the same properties.

--- src/main/java/continuum/essentials/mod/ObjectHolder.java
+++ src/main/java/continuum/essentials/mod/ObjectHolder.java
@@ -1,21 +1,22 @@
 package continuum.essentials.mod;
 
+import java.lang.reflect.Constructor;
+import java.util.List;
+
 import org.apache.commons.lang3.tuple.Pair;
 
-import continuum.essentials.hooks.ItemHooks.ItemBlockMeta;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
-public interface ObjectHolder
+public class ObjectHolder
 {
-	public String getModid();
+	private static final Constructor<Item> DEFAULT_CONSTRUCTOR;
 	
-	public String getName();
-	
-	public String getVersion();
-
 	public static <B extends Block> B newBlock(B block, String name, CreativeTabs tab)
 	{
 		block.setRegistryName(name);
@@ -33,7 +34,33 @@ public interface ObjectHolder
 			item.setCreativeTab(tab);
 		return item;
 	}
-
+	
+	public static List<Item> newItems(List<String> names, CreativeTabs tab)
+	{
+		return newItems(DEFAULT_CONSTRUCTOR, Lists.newArrayList(), names, tab);
+	}
+	
+	public static <I extends Item> List<I> newItems(Constructor<I> constructor, List<Object> constructor_args, List<String> names, CreativeTabs tab)
+	{
+		List<I> items = Lists.newArrayList();
+		String current = "null";
+		int remaining = names.size();
+		try
+		{
+			for(String name : names)
+			{
+				current = name;
+				items.add(newItem(constructor.newInstance(Iterables.toArray(constructor_args, Object.class)), name, tab));
+				remaining--;
+			}
+		}
+		catch(Exception exception)
+		{
+			throw new IllegalStateException("Failed To Construct Item \"" + current + "\" Due To A " + exception.getClass() + "; Had " + remaining + " Items Remaining.", exception);
+		}
+		return items;
+	}
+	
 	public static ItemBlock newItemBlock(Block block)
 	{
 		ItemBlock item = new ItemBlock(block);
@@ -41,15 +68,29 @@ public interface ObjectHolder
 		return item;
 	}
 	
+	public static List<ItemBlock> newItemBlocks(Block... blocks)
+	{
+		return newItemBlocks(Lists.newArrayList(blocks));
+	}
+	
+	public static List<ItemBlock> newItemBlocks(List<Block> blocks)
+	{
+		List<ItemBlock> item_blocks = Lists.newArrayList();
+		for(Block block : blocks)
+			item_blocks.add(newItemBlock(block));
+		return item_blocks;
+	}
+	
 	public static ItemBlock newItemBlock(Block block, int maxMeta)
 	{
 		ItemBlock item = new ItemBlock(block)
-				{
+		{
+			@Override
 			public int getMetadata(int meta)
 			{
 				return Math.min(meta, maxMeta);
 			}
-				};
+		};
 		item.setRegistryName(block.getRegistryName()).setHasSubtypes(true);
 		return item;
 	}
@@ -72,4 +113,15 @@ public interface ObjectHolder
 		return Pair.of(block, newItemBlock(block, meta));
 	}
 	
+	static
+	{
+		try
+		{
+			DEFAULT_CONSTRUCTOR = Item.class.getConstructor();
+		}
+		catch(Exception exception)
+		{
+			throw new IllegalStateException("Failed To Get Default Item Constructor.", exception);
+		}
+	}
 }
