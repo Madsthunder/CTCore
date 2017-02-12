d556162980537929877ecd89a5562f26bf09b896
Madsthunder <madsthunder109@gmail.com>
2017-02-12 15:22:45 -0500

    Update site.js

--- src/main/sites/utilities/site.js
+++ src/main/sites/utilities/site.js
@@ -1,11 +1,13 @@
 function initsite(resources)
 {
+	console.log(resources);
 	var http = getxmlhttp();
 	http.onreadystatechange = function(){if(this.readyState==4&&thid.status==200){document.body.innerHTML=this.responseText}};
 	sendrequest(http, "https://madsthunder.github.io/Continuum/src/main/sites/utilities/site.html");
 	document.getElementById("pagestyle").setAttribute("href", "https://madsthunder.github.io/Continuum/src/main/sites/utilities/site.css");
 	http.onreadystatechange = function(){if(this.readyState==4&&this.status==200){document.getElementById("titlebar").innerHTML=this.responseText}};
 	sendrequest(http, resources + "title.html");
+	console.log(resources);
 }
 
 function getxmlhttp()
