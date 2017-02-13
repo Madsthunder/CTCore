2d5831f59a0de181bb70edf98e91a33201c29d35
Madsthunder <madsthunder109@gmail.com>
2017-02-12 20:48:11 -0500

    Create modlist.js

---
+++ src/main/sites/utilities/modlist.js
@@ -0,0 +1,7 @@
+var modlist = 
+[
+"https://madsthunder.github.io/Continuum/src/main/resources/",
+"https://madsthunder.github.io/MoarWoods/src/main/resources/",
+"https://madsthunder.github.io/Restone-Logistics/src/main/resources/",
+"https://madsthunder.github.io/Metallurgic-Extras/src/main/resources/"
+]
