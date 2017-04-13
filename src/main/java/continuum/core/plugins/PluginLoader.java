2e8ffa0b4ce69a206d4a382f641f2548723ca92f
Madsthunder <madsthunder109@gmail.com>
2017-04-12 20:37:56 -0400

    Updated the PluginLoader to 1.11.2

--- src/main/java/continuum/core/plugins/PluginLoader.java
+++ src/main/java/continuum/core/plugins/PluginLoader.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.relauncher.IFMLCallHook;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.Name("Continuum: Core")
-@IFMLLoadingPlugin.MCVersion("1.11")
+@IFMLLoadingPlugin.MCVersion("1.11.2")
 @IFMLLoadingPlugin.TransformerExclusions({ "org", "com", "joptsimple", "oshi" })
 public class PluginLoader implements IFMLLoadingPlugin
 {
@@ -15,25 +15,25 @@ public class PluginLoader implements IFMLLoadingPlugin
 	{
 		return new String[] { "continuum.core.plugins.transformers.DebugEventTransformer" };
 	}
-
+	
 	@Override
 	public String getModContainerClass()
 	{
 		return "continuum.core.mod.CTCore_Mod";
 	}
-
+	
 	@Override
 	public String getSetupClass()
 	{
 		return null;
 	}
-
+	
 	@Override
 	public String getAccessTransformerClass()
 	{
 		return null;
 	}
-
+	
 	@Override
 	public void injectData(Map<String, Object> data)
 	{
